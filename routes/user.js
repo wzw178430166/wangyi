b6ec95f16591bcfbaca4072b696a2ed9b0b15efc
wzw178430166 <33919954+wzw178430166@users.noreply.github.com>
2019-05-26 01:25:47 +0800

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -8,17 +8,16 @@ var pool=require('../pool/pool.js');
  //添加路由（插入用户信息）
  router.post('/add',function(req,res){
 	 var obj1=req.body;
-	 console.log(obj1);
+	 //console.log(obj1);
 	 pool.query('INSERT INTO users SET?',[obj1],function(err,result){
 	  if(err) throw err;
-	 // console.log(result);
+	  console.log(result);
 	  if(result.affectedRows>0){
   res.send('添加成功');
  }else{
-  res.send('删除失败');
+  res.send('添加失败');
  }
 	});
-  
  });
 
 //用户登录 路由
