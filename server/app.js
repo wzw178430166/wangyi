a77dc9f0a38c7f63f25878dc44cd1b32310467d0
wzw178430166 <33919954+wzw178430166@users.noreply.github.com>
2019-06-21 21:28:45 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express=require('express');
-// 创建web服务器
+
+const cors=require('cors');
+
+//创建web服务器
 var server=express();
 server.listen(8080);
+
+//npm i -save cors
+server.use(cors({
+	origin:"http://127.0.0.1:5500"
+}))
+
 //引入mysql模块
 const mysql=require('mysql');
 //创建连接池
@@ -13,6 +22,9 @@ var pool=mysql.createPool({
 	 database:'tedu',
 	 connectionLimit:20 //设置连接池的大小
 });
+
+
+
 //引入第三方中间件body-parser 字符串查询中间件  post请求主体
 const bodyParser=require('body-parser');
 server.use(bodyParser.urlencoded({
@@ -22,11 +34,14 @@ server.use(bodyParser.urlencoded({
 
 //引入用户路由器
 const userRouter=require('../routes/user.js');
-//使用用户路由器 挂载： /user http://127.0.0.1:8080/user/add
-server.use('/user',userRouter);
+//引入ajax商品路由器
+const ajaxproduct=require('../routes/indexproduct');
+
 
 //(使用内置中间件)把静态资源托管到指定目录
 server.use(express.static('../public'));
-
-
+//使用用户路由器 挂载： /user http://127.0.0.1:8080/user
+server.use('/user',userRouter);
+//使用商品路由器 挂载： /http://127.0.0.1:8080/product
+server.use('/product',ajaxproduct);
 
